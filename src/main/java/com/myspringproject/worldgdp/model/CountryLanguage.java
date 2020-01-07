1575da34ba3c303001dc12a49053b745a4795cf0
liomotolani <ligaliomotolani@gmail.com>
2020-01-07 11:13:33 +0100

    first commit

---
+++ src/main/java/com/myspringproject/worldgdp/model/CountryLanguage.java
@@ -0,0 +1,33 @@
+package com.myspringproject.worldgdp.model;
+
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+
+@Data
+@Setter
+@Getter
+public class CountryLanguage {
+	
+	private Country country;
+	
+	@NotNull
+	private String countrycode;
+	
+	@NotNull
+	@Size(max= 30)
+	private String language;
+	
+	@NotNull
+	@Size(max= 1, min = 1)
+	private String isOfficial;
+	
+	@NotNull
+	private Double percentage;
+	
+	
+
+}
---
+++ src/main/java/com/myspringproject/worldgdp/model/Countrygdp.java
@@ -0,0 +1,15 @@
+package com.myspringproject.worldgdp.model;
+
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+
+@Data
+@Setter
+@Getter
+public class Countrygdp {
+	
+	private Short year;
+	private Double value;
+
+}
